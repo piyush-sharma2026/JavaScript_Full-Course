7aa0d7f777ca303d5a96c5ba9387d59f5647ecb7
Piyush Kumar Sh <piyushdeg9@gmail.com>
2025-09-06 22:12:31 +0000

    Story of const, let and var

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,40 @@
+const accountId = 14435;
+let accountEmail = "piyush22@gmail.com";
+var accountPassword = "12345";
+accountCity = "Jaipur";
+let accountState;
+
+
+
+// const variables cant be changed
+
+// accountId = 2; // Not allowed
+// console.log(accountId)
+
+
+
+
+
+// let variables can be changed
+
+// accountEmail = "Hitesh22@gmail.com"; // Allowed
+// console.log(accountEmail);
+
+
+// var variables can be changed
+accountPassword = "23534"; // Allowed
+
+
+accountCity = "Delhi";
+
+/*
+Note:- Prefer not to use var
+Because of issue in block scope and functional scope
+*/
+
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
+
+
+
+
