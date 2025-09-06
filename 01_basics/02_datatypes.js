e9ef2ae0410a766afa255ccb943beb7668f6e180
Piyush Kumar Sh <piyushdeg9@gmail.com>
2025-09-06 22:34:26 +0000

    Data Types

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,23 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3+3); // we are using nodejs not browser
+
+//Datatypes(Primitives)
+let name = "Piyush"; // String type
+let age = 18; // Number type
+let isLoggedIn = true; // Boolean type
+
+// number
+// bigint
+// string => "XYZ"
+// boolean => true/false
+// null => standalone value
+// undefined
+// symbol => unique
+
+//(object)
+console.log(typeof "Piyush"); // String
+console.log(typeof null);     // Object
+console.log(typeof undefined); // undefined
+
+
