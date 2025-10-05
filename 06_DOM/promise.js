f8a0f0c2d67c5acdd186481a481acc7b5dcaf9a4
Piyush Kumar Sh <piyushdeg9@gmail.com>
2025-10-05 13:05:05 +0000

    Document Object Model and selectors in DOM

--- 06_DOM/promise.js
+++
@@ -1,8 +0,0 @@
-// The promise object represents the eventual completion(or failure) of an asynchronous operation and its resulting value.
-/*
-A promise is in one of these states:
-       1.Pending
-       2.Fulfilled
-       3.Rejected
-*/
-
