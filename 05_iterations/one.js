409f93612d1e34f32b4fb793ce6856c05c3f3835
Piyush Kumar Sh <piyushdeg9@gmail.com>
2025-10-05 13:04:33 +0000

    iterations in js

--- 05_iterations/one.js
+++ 05_iterations/one.js
@@ -1,6 +1,6 @@
 // for loop
 
-for(let i=0;i<5;i++){
+for (let i = 0; i < 5; i++) {
     console.log("Hello World " + i);
 }
 //Output:- Hello World 0
@@ -11,7 +11,7 @@ for(let i=0;i<5;i++){
 
 // i++ is same as i = i + 1
 
-for(let i=0;i<=10;i++){
+for (let i = 0; i <= 10; i++) {
     console.log(i);
 }
 //Output:- 0
@@ -26,7 +26,7 @@ for(let i=0;i<=10;i++){
 //        9
 //        10
 
-for(let i=10;i>=0;i--){
+for (let i = 10; i >= 0; i--) {
     console.log(i);
 }
 //Output:- 10
@@ -41,7 +41,7 @@ for(let i=10;i>=0;i--){
 //        1
 //        0
 
-for(let i=0;i<=20;i=i+2){ // print even numbers from 0 to 20
+for (let i = 0; i <= 20; i = i + 2) { // print even numbers from 0 to 20
     console.log(i);
 }
 //Output:- 0
@@ -56,7 +56,7 @@ for(let i=0;i<=20;i=i+2){ // print even numbers from 0 to 20
 //        18
 //        20
 
-for(let i=1;i<=20;i=i+2){ // print odd numbers from 1 to 20
+for (let i = 1; i <= 20; i = i + 2) { // print odd numbers from 1 to 20
     console.log(i);
 }
 //Output:- 1
@@ -79,7 +79,7 @@ const person = {
     city: "New York"
 };
 
-for(let key in person){
+for (let key in person) {
     console.log(key + " : " + person[key]);
 }
 //Output:- name: John
@@ -93,7 +93,7 @@ for(let key in person){
 // for...of loop
 const colors = ["Red", "Green", "Blue"];
 
-for(let color of colors){
+for (let color of colors) {
     console.log(color);
 }
 //Output:- Red
@@ -105,9 +105,9 @@ for(let color of colors){
 
 
 // forEach loop
-const numbers = [1,2,3,4,5];
+const numbers = [1, 2, 3, 4, 5];
 
-numbers.forEach(function(arrElement){
+numbers.forEach(function (arrElement) {
     console.log(arrElement);
 });
 //Output:- 1
@@ -133,14 +133,14 @@ numbers.forEach((arrElement) => console.log(arrElement)); // Arrow function in f
 
 //nested for loop
 
- for(let i=0; i<=10; i++){
-    console.log(`Outer loop: " + ${i}`);
-    for(let j=0; j<=10; j++){
+for (let i = 0; i <= 10; i++) {
+    console.log(`Outer loop:  + ${i}`);
+    for (let j = 0; j <= 10; j++) {
         // 
-        
-        console.log(i + '*' + j + '=' + (i*j)); // multiplication table
- }
- }
+
+        console.log(i + '*' + j + '=' + (i * j)); // multiplication table
+    }
+}
 
 // Loops using arrays
 
@@ -159,10 +159,10 @@ numbers.forEach((arrElement) => console.log(arrElement)); // Arrow function in f
 
 // We can also use for...of loop to iterate over array
 
-    let myArray = ["flash", "Batman", "Superman"];
-    for(let hero of myArray){
+let myArray = ["flash", "Batman", "Superman"];
+for (let hero of myArray) {
     console.log(hero);
- }
+}
 // Output:- flast
 //         Batman
 //         Superman
@@ -171,19 +171,19 @@ numbers.forEach((arrElement) => console.log(arrElement)); // Arrow function in f
 
 
 // break and continue
-    for(let i=0; i<=10; i++){
+for (let i = 0; i <= 10; i++) {
 
-        if(i===5){
+    if (i === 5) {
         console.log("Breaking the loop at i = 5");
-            //break; // it will break the loop when i is 5
-            continue; // it will skip the iteration when i is 5 and continue with next iteration
-        }
-        
+        //break; // it will break the loop when i is 5
+        continue; // it will skip the iteration when i is 5 and continue with next iteration
+    }
 
-       console.log(`Value of i is: ${i}`);
-    }   
-    
-    
+
+    console.log(`Value of i is: ${i}`);
+}
+
+
 
 
 
