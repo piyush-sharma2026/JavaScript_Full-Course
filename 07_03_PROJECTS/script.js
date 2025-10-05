9049f5b7090f70c95d5888da1c016dddab94a756
Piyush Kumar Sh <piyushdeg9@gmail.com>
2025-10-05 14:44:16 +0000

    project 3 completed

---
+++ 07_03_PROJECTS/script.js
@@ -0,0 +1,7 @@
+document.getElementById('clock');
+const clock = document.getElementById('clock');
+
+let date = new Date();
+// console.log(date.toLocaleTimeString());
+clock.innerHTML = date.toLocaleTimeString();
+setInterval(function () { }, 1000);
